5e7463d884a58eeffaf8d27508add2f6e826df76
Christoph Leitner <christoph.leitner@student.tugraz.at>
2021-11-22 19:11:08 +0100

    IEEEtbme version

--- mtj_tracking/label/plot_widget.py
+++ mtj_tracking/label/plot_widget.py
@@ -7,7 +7,7 @@
 	
 	Please note: main.ui was compiled in open-source Qt (protected by GPLv3)
 
-	This program is free software: you can redistribute it and/or modify
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
--- mtj_tracking/train/model.py
+++ mtj_tracking/train/model.py
@@ -1,3 +1,24 @@
+"""
+	#deepMTJ
+	an open-source software tool made for biomechanical researchers
+
+	Copyright (C) 2021 by the authors: Jarolim Robert (University of Graz), <robert.jarolim@uni-graz.at> and
+	Leitner Christoph (Graz University of Technology), <christoph.leitner@tugraz.at>.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import glob
 import os
 
