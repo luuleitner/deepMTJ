9b671ea3c652770d4f20a5230bdccca7f1a6d05c
Christoph Leitner <chu.leitner@gmail.com>
2022-08-14 11:37:54 +0200

    updated package list

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ setup(
         'numpy>=1.18.4',
         'matplotlib>=3.1.3',
         'keras==2.3.1',
-        'keras-applications>=1.0.3'
-        'pandas>=1.0.3',
-        'tensorflow-gpu>=2.3.0,<=2.5.0',
+        'keras-applications>=1.0.3',
+        'pandas>=1.1.3',
+        'tensorflow-gpu==2.1.0',
         'opencv-python'],
 )
